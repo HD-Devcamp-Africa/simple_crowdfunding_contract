7fa3eb26875812efd23cc0b404162257b682a535
Mark Paul <markpaulbusiness66@gmail.com>
2025-03-20 16:26:58 +0300

    getting everything set up

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// Simple Crowd Funding Contract:
+
+// Features: Campaign creation, contribution tracking, deadline management.
+// Learning outcomes: Storage management, time-based conditions, payment handling.
+
+fn main() {
+    println!("Hello, world!");
+
+}
